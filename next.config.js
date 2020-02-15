bf9a5ca0ef69598cadcc865fe61c96d032de5af3
rakesh <rakeshtagadghar@gmail.com>
2020-02-15 14:07:22 +0530

    minify css

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
 const withCss = require('@zeit/next-css')
 const withPurgeCss = require('next-purgecss')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = withCss(
     withPurgeCss({
+       
+            webpack: (config, { isServer }) => {
+              if (config.mode === 'production') {
+                if (Array.isArray(config.optimization.minimizer)) {
+                  config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
+                }
+              }
+              return config;
+            },
+        
         purgeCssPaths: [
             'src/**/*'
           ],
